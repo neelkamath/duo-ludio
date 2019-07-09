30df8cd45561555304e4142b52a10f084a3f1e0d
neelkamath <neelkamath@icloud.com>
2019-07-09 10:45:37 +0530

    Enable use of native audio controls

--- src/ts/web_components/components/audio_player.ts
+++ src/ts/web_components/components/audio_player.ts
@@ -30,18 +30,40 @@ export default class AudioPlayerElement extends HTMLElement {
      */
     play(name: string, blob: Blob): void {
         while (this.shadowRoot!.firstChild) this.shadowRoot!.removeChild(this.shadowRoot!.firstChild!);
+        const control = this.getControl();
+        this.setUpAudio(control, blob);
+        const item = document.createElement('vaadin-item');
+        item.append(control, document.createTextNode(` ${name}`));
+        this.shadowRoot!.append(item);
+    }
+
+    private setUpAudio(control: AudioControlElement, blob: Blob): void {
         this.audio.loop = true;
         this.audio.src = URL.createObjectURL(blob);
+        this.audio.addEventListener('play', () => control.displaysPause = true);
+        this.audio.addEventListener('pause', () => control.displaysPause = false);
+
+        // Manipulate the audio so that it's at least five seconds long so it can be manipulated via native controls
+        this.audio.addEventListener('loadedmetadata', () => {
+            let duration = this.audio.duration;
+            const parts = [blob];
+            while (duration < 5) {
+                parts.push(blob);
+                duration += duration;
+            }
+            this.audio.src = URL.createObjectURL(new Blob(parts));
+            this.audio.play();
+        }, {once: true});
+    }
+
+    private getControl(): AudioControlElement {
         const control = document.createElement('audio-control') as AudioControlElement;
         control.displaysPause = true;
         control.addEventListener('click', () => {
             control.displaysPause ? this.audio.pause() : this.audio.play();
             control.displaysPause = !control.displaysPause;
         });
-        const item = document.createElement('vaadin-item');
-        item.append(control, document.createTextNode(` ${name}`));
-        this.shadowRoot!.append(item);
-        this.audio.play();
+        return control;
     }
 }
 
